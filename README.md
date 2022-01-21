211039d739af6c55cd6e9525a99a6526de3f2b81
Susul Liang <susulliang@gmail.com>
2022-01-21 06:15:45 +0000

    exhibition

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
-# realtime-gan
-Realtime VQGAN + CLIP, StyleGAN etc.
+# GANs
+realtime VQGAN + CLIP + etc.
+work in progress.
 
-Work in progress.
+# Exhibition Installation
+## Interactive modes
+- commmand prompt
+- webcam input (e.g. CAMO Webcam using iPhone 6s)
+- microphone speech input
+
+## Standby modes
+- local text prompts
+- local icon burn-ins
+- local image prompts
+
+## How to interact
+```
+python run.py
+```
+once the exhibition mode is running, it will enter loop synced at each second.
+Type anything in the console to enter user commmand mode
+
+Some examples of what you can prompt the AI to paint:
+```
+A boy holding an apple in abstract style
+The girl with a pearl earring
+Luncheon of the boating party
+Mars and Venus allegory of peace in black and white
+Adoration of the Magi
+The Last Supper in simplistic lines
+Watson and the shark
+A banquet with the skeleton in black and white
+Composition VIII
+Two lovers (The lovers)
+Big lemons on a tree with pink sky and blue clouds
+The Anatomy Lesson of Dr. Nicolaes Tulp
+View of Delft Townhall from the North 
+Cubist Animal Artworks
+Picasso paints his dreams
+Greek Mythology
+The Rialto Bridge and the Grand Canal in Venice
+View of Delft
+The steppe
+```
+
+Or type even in some styles to guide AI to a stylistic path. Examples (but not limited to):
+```
+styles = ["fish eye view", "dreaming", "Kandinsy", "Cubist", "Abstract",
+             "cartoon", "watercolor", "pixel", "pixelated", "simple", "pointillism", "acrylic", 
+             "Ghost in the Shell style", "Death Stranding style", "gouache", "vignetting",
+             "Dark and clean background", "small and center", "geometric", "linear", "Junji Ito style"]
+```
---
+++ exhibition.py
@@ -0,0 +1,84 @@
+import sys, os, traceback
+
+# -------------------------------------
+# -> ROOT_DIR should be on realtime-gan
+# -------------------------------------
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) 
+print(f'Launching from {ROOT_DIR}')
+sys.path.insert(0,f'{ROOT_DIR}/upscalers')
+# -------------------------------------
+
+
+
+from argparse import Namespace
+from BColors import BColors
+
+import gan_VQGanClip
+
+from handle_osc import OSCHandle
+from handle_cmd import CmdHandle
+
+
+import torch
+
+
+def main():
+
+    my_device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    
+    model_names_full = ("faceshq",
+                        "vqgan_imagenet_f16_16384",
+                        "wikiart_16384",
+                        "coco",
+                        "drin_transformer",
+                        "cin_transformer",
+                        "sflckr")
+    
+    tracking_img = "tdout_cam.jpg"
+
+    # --------------------                        
+    # -> Run Parameters
+    # --------------------    
+    vq_args = Namespace(model_names=["wikiart_16384"],
+        model_names_full=model_names_full,
+        load_from_pickle=True,
+        current_model_index=0,
+        textos="",
+        channel="",
+        ancho=384,
+        alto=384,
+        video_fps=60,
+        superres_factor=3,
+        step_size=0.25,
+        init_image=tracking_img,  
+        init_weight=0.05,
+        max_iteraciones=15,  # @param {type:"number"}
+        seed=5,  # @param {type:"number"}
+        prompts=[],
+        image_prompts="",
+        size="(256, 256)",
+        clip_model='ViT-B/32',
+        cutn=32,
+        cut_pow=1.,
+        display_freq=1,
+        device=my_device,
+        ramdisk="R:/",
+        video_name="default_mov_out",
+        video_interp_frames=4)
+
+    # Global variables for storing networks, models and tensors
+    vq = gan_VQGanClip.VQGanClip(args=vq_args)
+    
+    # -> Automated Command Prompt Mode
+    handle = CmdHandle(vq_ref=vq)
+    handle.exhibition()
+
+    return
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception:
+        traceback.print_exc()
+        exit()
